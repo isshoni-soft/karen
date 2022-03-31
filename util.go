5eb68b6590f050fbca7c5d76dd5df81824aeb906
Andrew <me@dragovorn.com>
2022-03-31 16:58:07 -0400

    add cast method

--- util.go
+++ util.go
@@ -4,3 +4,11 @@ func Zero[T any]() T {
 	var result T
 	return result
 }
+
+func Cast[T any](incoming any) T {
+	if v, ok := incoming.(T); ok {
+		return v
+	}
+
+	return Zero[T]()
+}
--- util_test.go
+++ util_test.go
@@ -2,10 +2,40 @@ package karen
 
 import "testing"
 
+type CastTest struct{}
+
+type FirstInterface interface {
+	First()
+}
+
+type SecondInterface interface {
+	Second()
+}
+
+func (c CastTest) First() {}
+
+func (c CastTest) Second() {}
+
 func TestZero(t *testing.T) {
 	var str string
 
 	if str != Zero[string]() {
 		t.Fatal("zero does not match default string")
+	}
+}
+
+func TestCast(t *testing.T) {
+	test := CastTest{}
+
+	if v := Cast[FirstInterface](test); v == nil {
+		t.Fatal("CastTest unable to be cast to FirstInterface")
+	}
+
+	if v := Cast[SecondInterface](test); v == nil {
+		t.Fatal("CastTest unable to be cast to SecondInterface")
+	}
+
+	if v := Cast[string](test); v != Zero[string]() {
+		t.Fatal()
 	}
 }
