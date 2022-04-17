f4f561d4bbf2860f4c97c2b0f564cf086a95e2c0
Andrew <me@dragovorn.com>
2022-04-17 10:55:11 -0400

    cleanup code & add IsEitherKind helper method

--- reflect.go
+++ reflect.go
@@ -2,7 +2,21 @@ package karen
 
 import "reflect"
 
+func IsEitherKind(object reflect.Value, kinds ...reflect.Kind) bool {
+	for _, k := range kinds {
+		if object.Kind() == k {
+			return true
+		}
+	}
+
+	return false
+}
+
 func FindFieldWithMatchingTag(object reflect.Value, key string, value string) reflect.Value {
+	if IsEitherKind(object, reflect.Interface, reflect.Pointer) {
+		object = object.Elem()
+	}
+
 	objectType := object.Type()
 	numFields := objectType.NumField()
 
--- reflect_test.go
+++ reflect_test.go
@@ -19,3 +19,21 @@ func TestFindFieldWithMatchingTag(t *testing.T) {
 		t.Fatal("did not get expected data from found field")
 	}
 }
+
+func TestIsEitherKind(t *testing.T) {
+	testPtr := &testStruct{
+		someField: "data",
+	}
+
+	test := testStruct{
+		someField: "data",
+	}
+
+	if IsEitherKind(reflect.ValueOf(test), reflect.Pointer, reflect.Interface) {
+		t.Fatal("detected ", test, " as either pointer or interface")
+	}
+
+	if !IsEitherKind(reflect.ValueOf(testPtr), reflect.Pointer, reflect.Interface) {
+		t.Fatal(test, " did not detect as either pointer or interface")
+	}
+}
